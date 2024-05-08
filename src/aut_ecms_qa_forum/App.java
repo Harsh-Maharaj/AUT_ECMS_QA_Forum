422a58ce5dd5a02d23360169cd95baab100dcbb7
Harsh Maharaj <HarshMaharaj969@gmail.com>
2024-05-08 18:22:57 +1200

    Main starting code

---
+++ src/aut_ecms_qa_forum/App.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aut_ecms_qa_forum;
+
+/**
+ *
+ * @author Harsh & Dillan
+ */
+public class App {
+    public static void main(String[] args) {
+        FilePersistence filePersistence = new FilePersistence(); // Create FilePersistence first since it is needed for QuestionBank
+        QuestionBank questionBank = new QuestionBank(filePersistence); // Pass it to the QuestionBank constructor
+        UserManager userManager = new UserManager();
+        SessionManager sessionManager = new SessionManager();
+
+        // If ConsoleUI's constructor expects these 4 arguments in this order and is implemented to handle them,
+        // then this part of your code is correct.
+        ConsoleUI ui = new ConsoleUI(questionBank, userManager, sessionManager, filePersistence);
+        ui.start();
+    }
+}
