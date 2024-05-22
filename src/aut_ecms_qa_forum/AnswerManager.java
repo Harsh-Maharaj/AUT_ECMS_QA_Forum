2b4c743ddee2b2236d924283b529c6e000a49af1
Harsh Maharaj <HarshMaharaj969@gmail.com>
2024-05-22 19:15:12 +1200

    refactor to work with database

--- src/aut_ecms_qa_forum/AnswerManager.java
+++ src/aut_ecms_qa_forum/AnswerManager.java
@@ -6,30 +6,53 @@ package aut_ecms_qa_forum;
  */
 
 
-
+import java.sql.*;
 import java.util.ArrayList;
-
-import java.util.ArrayList;
+import java.util.List;
 
 public class AnswerManager {
-    private ArrayList<Answer> answers = new ArrayList<>();
-
     public void addAnswer(Answer answer) {
-        answers.add(answer);
+        String sql = "INSERT INTO Answers (content, author, questionId) VALUES (?, ?, ?)";
+        try (Connection conn = DerbyDatabaseManager.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, answer.getContent());
+            pstmt.setString(2, answer.getAuthor().getUsername());
+            pstmt.setInt(3, answer.getQuestion().getId());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeAnswer(Answer answer) {
-        answers.remove(answer);
+        String sql = "DELETE FROM Answers WHERE id = ?";
+        try (Connection conn = DerbyDatabaseManager.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, answer.getId());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    public ArrayList<Answer> getAnswersByQuestion(Question question) {
-        ArrayList<Answer> result = new ArrayList<>();
-        for (Answer answer : answers) {
-            if (answer.getQuestion().equals(question)) {
-                result.add(answer);
+    public List<Answer> getAnswersByQuestion(Question question) {
+        List<Answer> answers = new ArrayList<>();
+        String sql = "SELECT * FROM Answers WHERE questionId = ?";
+        try (Connection conn = DerbyDatabaseManager.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, question.getId());
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String content = rs.getString("content");
+                String author = rs.getString("author");
+                User user = ForumDatabase.getInstance().getUserManager().authenticate(author, "");
+                answers.add(new Answer(id, content, user, question));
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        return result;
+        return answers;
     }
 }
 
