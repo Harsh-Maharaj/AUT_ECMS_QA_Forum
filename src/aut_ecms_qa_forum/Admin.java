d93c122d01fe94b12d48a181c43a16a82878b6fe
Harsh Maharaj <HarshMaharaj969@gmail.com>
2024-05-22 19:14:52 +1200

    Admin class update

--- src/aut_ecms_qa_forum/Admin.java
+++ src/aut_ecms_qa_forum/Admin.java
@@ -5,45 +5,39 @@ package aut_ecms_qa_forum;
 
 //token for harsh github: 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
 
+
 public class Admin extends User {
     public Admin(String username, String password) {
         super(username, password);
     }
 
-    // Method to add a question
     public void addQuestion(String title, String content, User author) {
         Question newQuestion = new Question(title, content, author);
         ForumDatabase.getInstance().getQuestionManager().addQuestion(newQuestion);
     }
 
-    // Method to edit a question
     public void editQuestion(Question question, String newTitle, String newContent) {
         question.setTitle(newTitle);
         question.setContent(newContent);
     }
 
-    // Method to delete a question
     public void deleteQuestion(Question question) {
         ForumDatabase.getInstance().getQuestionManager().removeQuestion(question);
     }
 
-    // Method to add an answer
     public void addAnswer(String content, User author, Question question) {
         Answer newAnswer = new Answer(content, author, question);
         ForumDatabase.getInstance().getAnswerManager().addAnswer(newAnswer);
     }
 
-    // Method to edit an answer
     public void editAnswer(Answer answer, String newContent) {
         answer.setContent(newContent);
     }
 
-    // Method to delete an answer
     public void deleteAnswer(Answer answer) {
         ForumDatabase.getInstance().getAnswerManager().removeAnswer(answer);
     }
 
-    // Method to add a user
     public void addUser(String username, String password, boolean isAdmin) {
         User newUser;
         if (isAdmin) {
@@ -54,7 +48,6 @@ public class Admin extends User {
         ForumDatabase.getInstance().getUserManager().addUser(newUser);
     }
 
-    // Method to delete a user
     public void deleteUser(User user) {
         ForumDatabase.getInstance().getUserManager().removeUser(user);
     }
