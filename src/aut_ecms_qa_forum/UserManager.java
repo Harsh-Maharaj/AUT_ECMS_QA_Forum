c3c8ff4d55972421093fb909821b55b12bcd145d
Harsh Maharaj <HarshMaharaj969@gmail.com>
2024-05-22 19:16:59 +1200

    User management has been updated to work with the database

--- src/aut_ecms_qa_forum/UserManager.java
+++ src/aut_ecms_qa_forum/UserManager.java
@@ -15,32 +15,66 @@ package aut_ecms_qa_forum;
  */
 
 
-
-import java.util.ArrayList;
-
+import java.sql.*;
 import java.util.ArrayList;
 
 public class UserManager {
-    private ArrayList<User> users = new ArrayList<>();
-
     public void addUser(User user) {
-        users.add(user);
+        String sql = "INSERT INTO Users (username, password, isAdmin) VALUES (?, ?, ?)";
+        try (Connection conn = DerbyDatabaseManager.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, user.getUsername());
+            pstmt.setString(2, user.getPassword());
+            pstmt.setBoolean(3, user instanceof Admin);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUser(User user) {
-        users.remove(user);
+        String sql = "DELETE FROM Users WHERE username = ?";
+        try (Connection conn = DerbyDatabaseManager.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, user.getUsername());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public User authenticate(String username, String password) {
-        for (User user : users) {
-            if (user.getUsername().equals(username) && user.checkPassword(password)) {
-                return user;
+        String sql = "SELECT * FROM Users WHERE username = ? AND password = ?";
+        try (Connection conn = DerbyDatabaseManager.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                boolean isAdmin = rs.getBoolean("isAdmin");
+                return isAdmin ? new Admin(username, password) : new User(username, password);
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return null;
     }
 
     public ArrayList<User> getAllUsers() {
+        ArrayList<User> users = new ArrayList<>();
+        String sql = "SELECT * FROM Users";
+        try (Connection conn = DerbyDatabaseManager.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                String username = rs.getString("username");
+                String password = rs.getString("password");
+                boolean isAdmin = rs.getBoolean("isAdmin");
+                users.add(isAdmin ? new Admin(username, password) : new User(username, password));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return users;
     }
 }
