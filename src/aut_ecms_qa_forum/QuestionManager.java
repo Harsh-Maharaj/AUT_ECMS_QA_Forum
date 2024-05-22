4a15ec751bb18dc3f2b5e56b5696d8f40f7c7353
Harsh Maharaj <HarshMaharaj969@gmail.com>
2024-05-22 19:16:24 +1200

    Questionmanager has been rafactored to work with database

--- src/aut_ecms_qa_forum/QuestionManager.java
+++ src/aut_ecms_qa_forum/QuestionManager.java
@@ -10,25 +10,52 @@ package aut_ecms_qa_forum;
  */
 
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class QuestionManager {
-    private List<Question> questions;
-
-    public QuestionManager() {
-        questions = new ArrayList<>();
-    }
-
     public void addQuestion(Question question) {
-        questions.add(question);
+        String sql = "INSERT INTO Questions (title, content, author) VALUES (?, ?, ?)";
+        try (Connection conn = DerbyDatabaseManager.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, question.getTitle());
+            pstmt.setString(2, question.getContent());
+            pstmt.setString(3, question.getAuthor().getUsername());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeQuestion(Question question) {
-        questions.remove(question);
+        String sql = "DELETE FROM Questions WHERE id = ?";
+        try (Connection conn = DerbyDatabaseManager.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, question.getId());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<Question> getAllQuestions() {
-        return new ArrayList<>(questions);
+        List<Question> questions = new ArrayList<>();
+        String sql = "SELECT * FROM Questions";
+        try (Connection conn = DerbyDatabaseManager.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String title = rs.getString("title");
+                String content = rs.getString("content");
+                String author = rs.getString("author");
+                User user = ForumDatabase.getInstance().getUserManager().authenticate(author, "");
+                questions.add(new Question(id, title, content, user));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return questions;
     }
 }
