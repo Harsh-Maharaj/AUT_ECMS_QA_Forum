321384d87e9a4afb2e4011690d49b2f2b787a6d6
Harsh Maharaj <harshmaharaj969@gmail.com>
2024-05-28 14:38:58 +1200

    Merge origin/main

--- Downloads/AUT_ECMS_QA_Forum-main (2)/AUT_ECMS_QA_Forum-main/src/aut_ecms_qa_forum/Admin.java
+++ Downloads/AUT_ECMS_QA_Forum-main (2)/AUT_ECMS_QA_Forum-main/src/aut_ecms_qa_forum/Admin.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 
 //token for harsh github: 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
 
-
+// testing for git purpose
 public class Admin extends User {
     public Admin(String username, String password) {
         super(username, password);
