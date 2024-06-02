d4b361ba5aa93862b432977acfa917fa30d40605
Harsh Maharaj <HarshMaharaj969@gmail.com>
2024-06-02 12:44:59 +1200

    ForumDatabase class fully commented

--- src/aut_ecms_qa_forum/ForumDatabase.java
+++ src/aut_ecms_qa_forum/ForumDatabase.java
@@ -7,21 +7,26 @@ package aut_ecms_qa_forum;
 import java.sql.SQLException;
 
 /**
- *
- * @author harsh
+ * The ForumDatabase class is a singleton that manages the UserManager, QuestionManager,
+ * and AnswerManager instances. It initializes the database and pre-loads some users.
+ * 
+ * @author Harsh
  */
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-
 public class ForumDatabase {
-    private static ForumDatabase instance;
-    private UserManager userManager;
-    private QuestionManager questionManager;
-    private AnswerManager answerManager;
+    private static ForumDatabase instance; // Singleton instance of the ForumDatabase
+    private UserManager userManager; // Manager for user-related operations
+    private QuestionManager questionManager; // Manager for question-related operations
+    private AnswerManager answerManager; // Manager for answer-related operations
 
+    /**
+     * Private constructor to prevent direct instantiation.
+     * Initializes the managers and the database, and pre-loads some users.
+     */
     private ForumDatabase() {
         userManager = new UserManager();
         questionManager = new QuestionManager();
@@ -32,6 +37,11 @@ public class ForumDatabase {
         initializeUsers();
     }
 
+    /**
+     * Gets the singleton instance of the ForumDatabase.
+     * 
+     * @return The singleton instance of the ForumDatabase
+     */
     public static synchronized ForumDatabase getInstance() {
         if (instance == null) {
             instance = new ForumDatabase();
@@ -39,6 +49,10 @@ public class ForumDatabase {
         return instance;
     }
 
+    /**
+     * Initializes some default users in the database.
+     * Adds an admin and a regular user if they do not already exist.
+     */
     private void initializeUsers() {
         try {
             if (!userManager.userExists("admin")) {
@@ -52,14 +66,29 @@ public class ForumDatabase {
         }
     }
 
+    /**
+     * Gets the UserManager instance.
+     * 
+     * @return The UserManager instance
+     */
     public UserManager getUserManager() {
         return userManager;
     }
 
+    /**
+     * Gets the QuestionManager instance.
+     * 
+     * @return The QuestionManager instance
+     */
     public QuestionManager getQuestionManager() {
         return questionManager;
     }
 
+    /**
+     * Gets the AnswerManager instance.
+     * 
+     * @return The AnswerManager instance
+     */
     public AnswerManager getAnswerManager() {
         return answerManager;
     }
