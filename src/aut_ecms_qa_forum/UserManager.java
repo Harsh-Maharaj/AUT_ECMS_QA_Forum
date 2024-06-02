8d8d6f476c5f7f22b3b6b781b9a553b7f25fa781
Harsh Maharaj <HarshMaharaj969@gmail.com>
2024-06-02 12:55:19 +1200

    UserManager class fully commented

--- src/aut_ecms_qa_forum/UserManager.java
+++ src/aut_ecms_qa_forum/UserManager.java
@@ -3,7 +3,18 @@ package aut_ecms_qa_forum;
 import java.sql.*;
 import java.util.ArrayList;
 
+/**
+ * The UserManager class provides methods to manage users in the forum.
+ * It includes methods to add, remove, authenticate, and retrieve users.
+ */
 public class UserManager {
+
+    /**
+     * Adds a new user to the database.
+     * 
+     * @param user The user to be added
+     * @throws SQLException If a database access error occurs
+     */
     public void addUser(User user) throws SQLException {
         if (!userExists(user.getUsername())) {
             String sql = "INSERT INTO Users (username, password, isAdmin) VALUES (?, ?, ?)";
@@ -19,6 +30,12 @@ public class UserManager {
         }
     }
 
+    /**
+     * Removes a user from the database.
+     * 
+     * @param user The user to be removed
+     * @throws SQLException If a database access error occurs
+     */
     public void removeUser(User user) throws SQLException {
         String sql = "DELETE FROM Users WHERE username = ?";
         try (Connection conn = DerbyDatabaseManager.getConnection();
@@ -30,6 +47,13 @@ public class UserManager {
         }
     }
 
+    /**
+     * Authenticates a user by checking the username and password.
+     * 
+     * @param username The username of the user
+     * @param password The password of the user
+     * @return The authenticated user, or null if authentication fails
+     */
     public User authenticate(String username, String password) {
         String sql = "SELECT * FROM Users WHERE username = ? AND password = ?";
         try (Connection conn = DerbyDatabaseManager.getConnection();
@@ -47,6 +71,12 @@ public class UserManager {
         return null;
     }
 
+    /**
+     * Retrieves a user by their username.
+     * 
+     * @param username The username of the user
+     * @return The user with the specified username, or null if no such user exists
+     */
     public User getUserByUsername(String username) {
         String sql = "SELECT * FROM Users WHERE username = ?";
         try (Connection conn = DerbyDatabaseManager.getConnection();
@@ -64,6 +94,13 @@ public class UserManager {
         return null;
     }
 
+    /**
+     * Checks if a user with the specified username exists in the database.
+     * 
+     * @param username The username to check
+     * @return True if the user exists, false otherwise
+     * @throws SQLException If a database access error occurs
+     */
     public boolean userExists(String username) throws SQLException {
         String sql = "SELECT COUNT(*) FROM Users WHERE username = ?";
         try (Connection conn = DerbyDatabaseManager.getConnection();
@@ -77,6 +114,11 @@ public class UserManager {
         return false;
     }
 
+    /**
+     * Retrieves all users from the database.
+     * 
+     * @return A list of all users
+     */
     public ArrayList<User> getAllUsers() {
         ArrayList<User> users = new ArrayList<>();
         String sql = "SELECT * FROM Users";
