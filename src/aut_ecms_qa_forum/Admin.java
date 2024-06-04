8be1e78a9b5a74fe7ec5084a6f338f5ab6f859ca
Harsh Maharaj <72852086+Harsh-Maharaj@users.noreply.github.com>
2024-06-05 11:55:04 +1200

    Update Admin.java

--- src/aut_ecms_qa_forum/Admin.java
+++ src/aut_ecms_qa_forum/Admin.java
@@ -10,7 +10,6 @@ import java.sql.SQLException;
 
 // Check admin class
 
-// Token for harsh github: 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
 
 // Testing purposes for Branch merging
 public class Admin extends User {
